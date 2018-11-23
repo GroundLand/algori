94b085d9dd389329081c1cee240e192606bc4244
cln <cln@servyou.com.cn>
2018-11-23 21:38:54 +0800

    格式修改

--- src/main/java/com/algori/algri4/chapterOne/first/BinarySearch.java
+++ src/main/java/com/algori/algri4/chapterOne/first/BinarySearch.java
@@ -6,19 +6,20 @@ package com.algori.algri4.chapterOne.first;
  * @version $Id: BinarySearch.java v 0.1 18-11-23 下午9:11 cln Exp $$
  */
 public class BinarySearch {
-    public static int rank(int key,int[] a){
+    public static int rank(int key, int[] a) {
         int lo = 0;
-        int hi = a.length-1;
-        while (lo<hi){
-            int mid = (hi+lo)/2;
+        int hi = a.length - 1;
+        while (lo < hi) {
+            int mid = (hi + lo) / 2;
             // 这里为什么要减一呢?
             // 完美的避免如果是当前值的左边正好是这个值的情况(减少循环),加一也是
-            if (key<a[mid]) hi = mid-1;
-            else if (key>a[mid]) lo=mid+1;
-            else return mid;
+            if (key < a[mid])
+                hi = mid - 1;
+            else if (key > a[mid])
+                lo = mid + 1;
+            else
+                return mid;
         }
         return -1;
     }
-
-
 }
