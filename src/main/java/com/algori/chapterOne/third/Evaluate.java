655834575df23cb9d6f3fc3f721a761dfeb40e1a
cln <cln@servyou.com.cn>
2018-12-08 13:47:35 +0800

    练习题1.3.4

---
+++ src/main/java/com/algori/chapterOne/third/Evaluate.java
@@ -0,0 +1,39 @@
+package com.algori.algri4.chapterOne.third;
+
+import edu.princeton.cs.algs4.Stack;
+
+/**
+ * @author cln
+ * @version $Id: Evaluate.java v 0.1 18-12-8 上午10:10 cln Exp $$
+ */
+public class Evaluate {
+
+    public static void main(String[] args) {
+        String[] strs = "(1+((2+3)*(4*5)))".split("");
+
+        Stack<String> ops = new Stack<>();
+        Stack<Double> vals = new Stack<>();
+        int i = 0;
+        while (i<strs.length) {
+            //如果读取的是字符串
+            String s = strs[i];
+            if (s.equals("(")) ;
+            else if (s.equals("+")) ops.push(s);
+            else if (s.equals("-")) ops.push(s);
+            else if (s.equals("*")) ops.push(s);
+            else if (s.equals("/")) ops.push(s);
+            else if (s.equals("sqrt")) ops.push(s);
+            else if (s.equals(")")) {
+                String op = ops.pop();
+                double v = vals.pop();
+                if (op.equals("+")) v = vals.pop() + v;
+                else if (op.equals("-")) v = vals.pop() - v;
+                else if (op.equals("*")) v = vals.pop() * v;
+                else if (op.equals("/")) v = vals.pop() / v;
+                vals.push(v);
+            } else vals.push(Double.parseDouble(s));
+            i++;
+        }
+        System.out.println(vals);
+    }
+}
---
+++ src/main/java/com/algori/chapterOne/third/Parentheses.java
@@ -0,0 +1,8 @@
+package com.algori.chapterOne.third;
+
+/**
+ * @author cln
+ * @version $Id: Parentheses.java v 0.1 18-12-8 下午12:52 cln Exp $$
+ */
+public class Parentheses {
+}
